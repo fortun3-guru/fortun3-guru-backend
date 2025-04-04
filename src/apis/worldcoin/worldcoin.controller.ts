ce0904195466cc28605ed10c6f20f20cbd355b00
Ikhalas Mannoon <ikhalas.work@gmail.com>
2025-04-05 02:33:43 +0800

    update worldcoin api

--- src/apis/worldcoin/worldcoin.controller.ts
+++ src/apis/worldcoin/worldcoin.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { WorldcoinService } from './worldcoin.service';
-import { MiniAppWalletAuthSuccessPayload } from '@worldcoin/minikit-js/*';
+import {
+  MiniAppPaymentSuccessPayload,
+  MiniAppWalletAuthSuccessPayload,
+} from '@worldcoin/minikit-js/*';
 
 @Controller('worldcoin')
 export class WorldcoinController {
@@ -16,5 +19,12 @@ export class WorldcoinController {
     @Body() body: { payload: MiniAppWalletAuthSuccessPayload; nonce: string },
   ) {
     return this.worldcoinService.verifySiweMessage(body.payload, body.nonce);
+  }
+
+  @Post('confirm-payment')
+  async confirmPayment(
+    @Body() body: { payload: MiniAppPaymentSuccessPayload; nonce: string },
+  ) {
+    return this.worldcoinService.confirmPayment(body.payload, body.nonce);
   }
 }
--- src/apis/worldcoin/worldcoin.service.ts
+++ src/apis/worldcoin/worldcoin.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import {
   MiniAppWalletAuthSuccessPayload,
   verifySiweMessage,
+  MiniAppPaymentSuccessPayload,
 } from '@worldcoin/minikit-js';
 
 @Injectable()
@@ -16,5 +17,27 @@ export class WorldcoinService {
 
   async nonce() {
     return crypto.randomUUID().replace(/-/g, '');
+  }
+
+  async confirmPayment(payload: MiniAppPaymentSuccessPayload, nonce: string) {
+    if (payload.reference !== nonce) {
+      throw new BadRequestException('Invalid nonce');
+    }
+
+    const response = await fetch(
+      `https://developer.worldcoin.org/api/v2/minikit/transaction/${payload.transaction_id}?app_id=${process.env.WORLDCOIN_APP_ID}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${process.env.WORLDCOIN_DEV_PORTAL_API_KEY}`,
+        },
+      },
+    );
+    const transaction = await response.json();
+    if (transaction.reference === nonce && transaction.status != 'failed') {
+      return { success: true };
+    } else {
+      return { success: false };
+    }
   }
 }
